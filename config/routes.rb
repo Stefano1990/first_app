2c7c7616406be745060298cd2352bccf40729abe
Stefano Hug <stefano.hug@gmail.com>
2010-08-07 20:54:17 +0200

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 FirstApp::Application.routes.draw do
-  
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => "pages#home"
   
   match "/signup",   :to => 'users#new'
+  
+  match "/signin",   :to => "sessions#new"
+  match "/signout",  :to => "sessions#destroy"
+   
   match "/about",    :to => 'pages#about'
   match "/contact",  :to => 'pages#contact'
   match "/help",     :to => 'pages#help'
